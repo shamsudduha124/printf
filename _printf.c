00503c7e50cad3ce1cef2070f06b893eea030da8
shamsudduha124 <shamsudduha124@gmail.com>
2023-09-18 10:29:19 -0700

    _printf

--- _printf.c
+++ _printf.c
@@ -12,22 +12,27 @@
  */
 int _printf(const char *format, ...)
 {
-	int (*pfunc)(va_list, flags_t *);
+	int (*pfunc)(va_list, log_t *);
 	const char *p;
 	va_list arguments;
-	flags_t flags = {0, 0, 0};
+	log_t flags = {0, 0, 0};
 
 	register int count = 0;
 
 	va_start(arguments, format);
 	if (!format || (format[0] == '%' && !format[1]))
+	{
 		return (-1);
+	}
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
+	{
 		return (-1);
+	}
 	for (p = format; *p; p++)
 	{
-		if (*p == '%')
+		switch (*p)
 		{
+		case '%':
 			p++;
 			if (*p == '%')
 			{
@@ -35,16 +40,19 @@ int _printf(const char *format, ...)
 				continue;
 			}
 			while (get_flag(*p, &flags))
+			{
 				p++;
+			}
 			pfunc = get_print(*p);
-			count += (pfunc)
-				? pfunc(arguments, &flags)
-				: _printf("%%%c", *p);
-		} else
+			count += (pfunc) ? pfunc(arguments, &flags) : _printf("%%%c", *p);
+			break;
+
+		default:
 			count += _putchar(*p);
+			break;
+		}
 	}
 	_putchar(-1);
 	va_end(arguments);
 	return (count);
-
 }
