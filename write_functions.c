6d4c3d39e2229efd69ad37fdef1cdd83b6fb3604
Busib22 <139709137+Busib22@users.noreply.github.com>
2023-09-19 16:01:32 +0200

    Update write_functions.c

--- write_functions.c
+++ write_functions.c
@@ -11,17 +11,17 @@
  */
 int _putchar(char c)
 {
-	static char buf[1024];
+	static char buffy[1024];
 	static int i;
 
 	if (c == -1 || i >= 1024)
 	{
-		write(1, &buf, i);
+		write(1, &buffy, i);
 		i = 0;
 	}
 	if (c != -1)
 	{
-		buf[i] = c;
+		buffy[i] = c;
 		i++;
 	}
 	return (1);
