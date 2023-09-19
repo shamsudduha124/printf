96048f7d136a1ddde5d27e8f6342a3d10f7d9eb9
shamsudduha124 <shamsudduha124@gmail.com>
2023-09-19 03:51:42 -0700

    tetsing

--- _printf.c
+++ _printf.c
@@ -9,44 +9,57 @@
  */
 int _printf(const char *format, ...)
 {
-	int (*pfunc)(va_list, log_t *);
-	const char *p;
-	va_list arguments;
-	log_t flags = {0, 0, 0};
+    int (*pfunc)(va_list, log_t *);
+    const char *p;
+    va_list arguments;
+    log_t flags = {0, 0, 0};
 
-	register int count = 0;
+    register int count = 0;
 
-	va_start(arguments, format);
+    va_start(arguments, format);
+    if (!format || (format[0] == '%' && !format[1]))
+    {
+        return (-1);
+    }
+    if (format[0] == '%' && format[1] == ' ' && !format[2])
+    {
+        return (-1);
+    }
+    for (p = format; *p; p++)
+    {
+        switch (*p)
+        {
+        case '%':
+            p++;
+            if (*p == '%')
+            {
+                count += _putchar('%');
+                continue;
+            }
+            while (get_flag(*p, &flags))
+            {
+                p++;
+            }
+            pfunc = get_print(*p);
+            if (pfunc)
+            {
+                count += pfunc(arguments, &flags);
+            }
+            else
+            {
+                // Unsupported specifier, print it as-is
+                count += _putchar('%');
+                count += _putchar(*p);
+            }
+            break;
 
-	if (!format || (format[0] == '%' && !format[1]))
-	{
-		return (-1);
-	}
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-	{
-		return (-1);
-	}
-	for (p = format; *p; p++)
-	{
-		if (*p == '%')
-		{
-			p++;
-			if (*p == '%')
-			{
-				count += _putchar('%');
-				continue;
-			}
-			while (get_flag(*p, &flags))
-				p++;
-			pfunc = get_print(*p);
-			count += (pfunc)
-				? pfunc(arguments, &flags)
-				: _printf("%%%c", *p);
-		} else
-			count += _putchar(*p);
-	}
-	_putchar(-1);
-	va_end(arguments);
-	return (count);
-
+        default:
+            count += _putchar(*p);
+            break;
+        }
+    }
+    _putchar(-1);
+    va_end(arguments);
+    return (count);
 }
+
