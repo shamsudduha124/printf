98f75bc79c0c8180a3b74ce819ebd1885fe1f905
shamsudduha124 <shamsudduha124@gmail.com>
2023-09-19 00:22:20 -0700

    print_percent.c

---
+++ print_percent.c
@@ -0,0 +1,12 @@
+/**
+ * print_percent - prints a percent
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct flags in which we turn the flags on
+ * Return: number of char printed
+ */
+int print_percent(va_list l, log_t *f)
+{
+	(void)l;
+	(void)f;
+	return (_putchar('%'));
+}
