f210515c003db828b3b1f9f3b49d121781ccc1a2
shamsudduha124 <shamsudduha124@gmail.com>
2023-09-19 07:05:03 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,32 +1,59 @@
-#include <unistd.h>
-#include <stdarg.h>
-#include <stdio.h>
 #include "main.h"
+#include <limits.h>
+#include <stdio.h>
 
 /**
-<<<<<<< HEAD
- * _printf - Produces output according to a format
- * @format: The format of the string.
- * Return: The number of characters printed
-=======
  * _printf - produces output according to a format
  * @format: format string containing the characters and the specifiers
- * Description: this function will call the get_print() function that wil
+ * Description: this function will call the get_print() function that will
+ * determine which printing function to call depending on the conversion
  * specifiers contained into fmt
  * Return: length of the formatted output string
->>>>>>> 670c1afc96bd2245cebf9ce831b68c84ac5b6e44
  */
 int _printf(const char *format, ...)
 {
-	unsigned int i = 0, n = 0;
+	int (*pfunc)(va_list, log_t *);
+	const char *p;
+	va_list arguments;
+	log_t flags = {0, 0, 0};
 
-	while (format[i] != 0)
+	register int count = 0;
+
+	va_start(arguments, format);
+	if (!format || (format[0] == '%' && !format[1]))
 	{
-		if (format[i] == '%')
-			n++;
-		i++;
+		return (-1);
 	}
-	write(1, format, n);
-	printf("We have %d formatters\n", n);
-	return (n);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+	{
+		return (-1);
+	}
+	for (p = format; *p; p++)
+	{
+		switch (*p)
+		{
+		case '%':
+			p++;
+			if (*p == '%')
+			{
+				count += _putchar('%');
+				continue;
+			}
+			while (get_flag(*p, &flags))
+			{
+				p++;
+			}
+			pfunc = get_print(*p);
+			count += (pfunc) ? pfunc(arguments, &flags) : _printf("%%%c", *p);
+			break;
+
+		default:
+			count += _putchar(*p);
+			break;
+		}
+	}
+	_putchar(-1);
+	va_end(arguments);
+	return (count);
+
 }
