e9204f9398ec79e418008c5e4d92689414237ba5
shamsudduha <shamsudduha124@gmail.com>
2023-09-20 07:00:19 +0100

    Update write_functions.c putchar wahala

--- write_functions.c
+++ write_functions.c
@@ -2,32 +2,6 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- * Description: _putchar uses a local buffer of 1024 to call write
- * as little as possible
- */
-int _putchar(char c)
-{
-	static char buffy[1024];
-	static int i;
-
-	if (c == -1 || i >= 1024)
-	{
-		write(1, &buffy, i);
-		i = 0;
-	}
-	if (c != -1)
-	{
-		buffy[i] = c;
-		i++;
-	}
-	return (1);
-}
-
-/**
  * _puts - prints a string to stdout
  * @str: pointer to the string to print
  * Return: number of chars written
