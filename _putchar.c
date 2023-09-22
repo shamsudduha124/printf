f2fa6d40d52ac266f0ee6282508566b8e16b8d31
Busib22 <139709137+Busib22@users.noreply.github.com>
2023-09-22 11:56:09 +0200

    Update _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _puts - prints a string with newline
- * @str: the string to print
+ * _puts - prints a string witnewline
+ * @str: the string to prints
  *
  * Return:( str-a)
  */
